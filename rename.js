83c7feaa9dfeaf5509217f795606a4909e6d40ea
dpartner <d.partner.ua@gmail.com>
2022-01-29 19:33:28 +0200

    убрал лишнее

--- rename.js
+++ rename.js
@@ -4,15 +4,6 @@ var path = require('path');
 const srcFolder = path.resolve(__dirname, './srcImage') + '/';
 const outFolder = path.resolve(__dirname, './outImage') + '/';
 
-// var d = new Date();
-// var curr_date = d.getDate();
-// var curr_month = d.getMonth() + 1;
-// var curr_year = d.getFullYear();
-
-// var data_f = curr_date + '-' + curr_month + '-' + curr_year;
-// var myPrefix = '-t-t-t-';
-// var prefix = myPrefix + data_f;
-
 fs.readdir(srcFolder, (err, files) => {
   files.forEach((file, i) => {
     let extension = path.extname(file);
